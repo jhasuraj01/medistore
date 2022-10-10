3cd37812a7a1aae05b55c6ebc3e04db3a38af3cb
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2022-10-10 13:19:11 +0000

    sidenav ready

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,9 +1,20 @@
 import style from './index.module.scss'
+import { NavLinkPersist } from '../../supports/Persistence'
+import { ReactComponent as PlaceHolderIcon } from '../../icons/placeholder.svg'
 
 export function Nav() {
   return (
     <nav className={style.nav}>
-      I am nav bar!!
+      <div className={style.navSection}>
+        <button className={style.navOption}><PlaceHolderIcon /></button>
+        <NavLinkPersist className={style.navOption} to='/'><PlaceHolderIcon /></NavLinkPersist>
+        <NavLinkPersist className={style.navOption} to='/search'><PlaceHolderIcon /></NavLinkPersist>
+        <NavLinkPersist className={style.navOption} to='/sales'><PlaceHolderIcon /></NavLinkPersist>
+        <NavLinkPersist className={style.navOption} to='/store'><PlaceHolderIcon /></NavLinkPersist>
+      </div>
+      <div className={style.navSection}>
+        <div className={style.navOption}><PlaceHolderIcon /></div>
+      </div>
     </nav>
   )
 }
