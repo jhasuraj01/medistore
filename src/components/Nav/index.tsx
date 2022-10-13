3cf5928c2d55e2901e74891c0f94c47263823fb8
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2022-10-13 18:04:34 +0000

    updated nav icons

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,19 +1,26 @@
 import style from './index.module.scss'
 import { NavLinkPersist } from '../../supports/Persistence'
-import { ReactComponent as PlaceHolderIcon } from '../../icons/placeholder.svg'
+// import { ReactComponent as PlaceHolderIcon } from '../../icons/placeholder.svg'
+import { ReactComponent as Home } from '../../icons/home.svg'
+import { ReactComponent as Menu } from '../../icons/menu.svg'
+import { ReactComponent as Sales } from '../../icons/sales.svg'
+import { ReactComponent as Search } from '../../icons/search.svg'
+import { ReactComponent as Store } from '../../icons/store.svg'
+import { ReactComponent as Logo } from '../../icons/logo.svg'
+import navData from '../../data/nav'
 
 export function Nav() {
   return (
     <nav className={style.nav}>
       <div className={style.navSection}>
-        <button className={style.navOption}><PlaceHolderIcon /></button>
-        <NavLinkPersist className={style.navOption} to='/'><PlaceHolderIcon /></NavLinkPersist>
-        <NavLinkPersist className={style.navOption} to='/search'><PlaceHolderIcon /></NavLinkPersist>
-        <NavLinkPersist className={style.navOption} to='/sales'><PlaceHolderIcon /></NavLinkPersist>
-        <NavLinkPersist className={style.navOption} to='/store'><PlaceHolderIcon /></NavLinkPersist>
+        <button className={style.navOption}><Menu /></button>
+        <NavLinkPersist className={style.navOption} to={`/${navData.home.upperOptions[0].link}`} ><Home /></NavLinkPersist>
+        <NavLinkPersist className={style.navOption} to={`/search/${navData.search.upperOptions[0].link}`} ><Search /></NavLinkPersist>
+        <NavLinkPersist className={style.navOption} to={`/sales/${navData.sales.upperOptions[0].link}`} ><Sales /></NavLinkPersist>
+        <NavLinkPersist className={style.navOption} to={`/store/${navData.store.upperOptions[0].link}`} ><Store /></NavLinkPersist>
       </div>
       <div className={style.navSection}>
-        <div className={style.navOption}><PlaceHolderIcon /></div>
+        <div className={style.navOption}><Logo /></div>
       </div>
     </nav>
   )
