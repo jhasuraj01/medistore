959488f8edf23743da49e0284e87f440d63e5791
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2022-10-21 12:17:23 +0000

    integration done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,20 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@apollo/server/express4';
+import path from "path";
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+import * as dotenv from 'dotenv';
+dotenv.config()
+
 const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
     author: String
   }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     books: [Book]
   }
@@ -32,27 +31,42 @@ const books = [
   },
 ];
 
-// Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
     books: () => books,
   },
 };
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
+interface MyContext {
+  token?: String;
+}
+
+const app = express();
+const httpServer = http.createServer(app);
+const server = new ApolloServer<MyContext>({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+await server.start();
+app.use(express.static(path.resolve('../', 'client/build')));
+
+app.get("*", (req, res, next) => {
+  if(req.path == '/graphql') next();
+  else res.sendFile(path.resolve('../', 'client/build', 'index.html'));
 });
 
-console.log(`🚀  Server ready at: ${url}`);
+app.use(
+  '/graphql',
+  cors<cors.CorsRequest>(),
+  bodyParser.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  }),
+);
+
+// Modified server startup
+const port:Number = Number(process.env.PORT) || 4000;
+await new Promise<void>((resolve) => httpServer.listen({ port: port }, resolve));
+console.log(`🚀 Server ready at http://localhost:${port}/`);
