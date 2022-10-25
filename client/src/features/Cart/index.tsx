95379e303140265d8cd4e73d113a80b61d1f94cb
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2022-10-25 13:39:29 +0000

    create new carts

---
+++ client/src/features/Cart/index.tsx
@@ -0,0 +1,24 @@
+import { useParams } from 'react-router-dom'
+import { NotFoundPage } from '../../pages/404'
+
+/**
+ * Validate Cart ID
+ * @todo Update cart ID validation logic
+ */
+const isValidID = (id: string) => {
+  return (new Date(Number(id))).getTime() > 0
+}
+
+export function Cart() {
+  const { id } = useParams()
+
+  console.log(id)
+
+  if(!id || !isValidID(id)) {
+    return <NotFoundPage />
+  }
+
+  return (
+    <p>Cart: {id}</p>
+  )
+}
--- client/src/pages/carts/index.tsx
+++ client/src/pages/carts/index.tsx
@@ -1,23 +1,27 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { AppSectionLayout } from '../../components/AppSectionLayout'
+import { Cart } from '../../features/Cart'
 import { SubNav, SubNavButton, SubNavLink, SubNavSection } from '../../features/SubNav'
-import { NavigatePersist } from '../../supports/Persistence'
+import { NavigatePersist, useNavigatePersist } from '../../supports/Persistence'
 import { NotFoundPage } from '../404'
 
 function CartPageSubNav() {
   const [carts, setCarts] = useState<string[]>([])
+  const navigate = useNavigatePersist()
 
   const createNewBill = () => {
-    setCarts([String(Date.now()), ...carts])
+    const id = String(Date.now())
+    setCarts([id, ...carts])
+    navigate(id)
   }
 
   return (
-    <SubNav title='Search'>
+    <SubNav title='Carts'>
       <SubNavSection>
         <SubNavButton onClick={createNewBill}>Create New Cart</SubNavButton>
         { carts.map(cart => <SubNavLink key={cart} to={cart}>{cart}</SubNavLink>)}
-        <SubNavLink to='Sample Cart'>Sample Bill</SubNavLink>
+        <SubNavLink to='sample-cart'>Sample Bill</SubNavLink>
       </SubNavSection>
     </SubNav>
   )
@@ -28,6 +32,7 @@ export function CartPage() {
     <Routes>
       <Route path='/' element={<AppSectionLayout subnav={<CartPageSubNav />}  />} >
         <Route index element={<NavigatePersist to='create-new-bill' />} />
+        <Route path=':id' element={<Cart />} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     </Routes>
