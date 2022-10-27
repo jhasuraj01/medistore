fc842ee9c4eb3fef1e17ddabdb541ec219f7fada
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2022-10-27 20:13:31 +0000

    started creating stock management page UI

--- client/src/pages/store/stock/index.tsx
+++ client/src/pages/store/stock/index.tsx
@@ -1,28 +1,50 @@
-import { Table, TableBody, TableFooter, TableHead } from '../../../components/Table'
+import { Table, TableBody, TableHead } from '../../../components/Table'
 import styles from './index.module.scss'
+
+const stock = [
+  {
+    id: '123456',
+    name: 'Detol HandWash',
+    quantity: 50,
+    costPerUnit: 85,
+    pricePerUnit: 100,
+    discount: 0.1
+  },
+]
 
 export function StockPage() {
   return (
     <div className={styles.container}>
-      <h2>Stock Management Page</h2>
-      <Table className={styles.table}>
+      <div className={styles.header}>Stock Management Page</div>
+      <Table className={styles.stockTable}>
         <TableHead>
           <th>#</th>
+          <th>ID</th>
           <th>Name</th>
           <th>Quantity</th>
+          <th>Cost / Unit</th>
+          <th>Price / Unit</th>
+          <th>Margin</th>
+          <th>Discount</th>
+          <th>PL / Unit</th>
         </TableHead>
         <TableBody>
-          <tr>
-            <td>1</td>
-            <td>Detol Hand Wash</td>
-            <td>50</td>
-          </tr>
+          {
+            stock.map((item, index) => (
+              <tr key={item.id}>
+                <td>{index + 1}</td>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.quantity}</td>
+                <td>{item.costPerUnit}</td>
+                <td>{item.pricePerUnit}</td>
+                <td>{item.pricePerUnit - item.costPerUnit}</td>
+                <td>{item.discount}</td>
+                <td>{item.pricePerUnit * (1 - item.discount) - item.costPerUnit}</td>
+              </tr>
+            ))
+          }
         </TableBody>
-        <TableFooter>
-          <td></td>
-          <td></td>
-          <td>50</td>
-        </TableFooter>
       </Table>
     </div>
   )
