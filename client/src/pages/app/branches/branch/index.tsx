da5db49175158b6b60cbea2931cd9544fd0e4ee6
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2022-11-09 10:29:29 +0000

    update Item

--- client/src/pages/app/branches/branch/index.tsx
+++ client/src/pages/app/branches/branch/index.tsx
@@ -5,20 +5,25 @@ import { Route, Routes, useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import { IconButton } from '../../../../components/IconButton'
 import { Table, TableBody, TableHead } from '../../../../components/Table'
-import { AddItemMutation, AddItemMutationVariables, DeleteItemMutation, DeleteItemMutationVariables, GetItemsQuery, GetItemsQueryVariables } from '../../../../gql/graphql'
+import { AddItemMutation, AddItemMutationVariables, DeleteItemMutation, DeleteItemMutationVariables, GetItemsQuery, GetItemsQueryVariables, UpdateItemMutation, UpdateItemMutationVariables } from '../../../../gql/graphql'
 import { NavLinkPersist } from '../../../../supports/Persistence'
 import { NotFoundPage } from '../../../404'
 import { BranchProxy } from './branchProxy'
 import styles from './index.module.scss'
 import { ReactComponent as TrashIcon } from '../../../../icons/trash.svg'
 
-const sleep = (sec: number): Promise<boolean> => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      res(true)
-    }, sec*1000)
-  })
-}
+const currency = new Intl.NumberFormat('en-IN', {
+  style: 'currency',
+  currency: 'INR',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
+
+const percent = new Intl.NumberFormat('en-IN', {
+  style: 'percent',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
 
 const queuePromise = async (func: (() => Promise<unknown>)[]): Promise<void> => {
   for (let i = 0; i < func.length; i++) {
@@ -57,6 +62,12 @@ const GET_ITEMS = gql`
 const ADD_ITEM = gql`
   mutation AddItem($organizationId: ID!, $branchId: ID!, $addItemId: ID!, $brandName: StringNonEmpty!, $companyName: StringNonEmpty!, $quantity: IntPositive!, $costPerUnit: FloatNonNegative!, $pricePerUnit: FloatNonNegative!, $manufactureAt: DatePast!, $discount: Float0To1, $expireAt: DateFuture) {
     addItem(organizationId: $organizationId, branchId: $branchId, id: $addItemId, brandName: $brandName, companyName: $companyName, quantity: $quantity, costPerUnit: $costPerUnit, pricePerUnit: $pricePerUnit, manufactureAt: $manufactureAt, discount: $discount, expireAt: $expireAt)
+  }
+`
+
+const UPDATE_ITEM = gql`
+  mutation UpdateItem($organizationId: ID!, $branchId: ID!, $updateItemId: ID!, $brandName: StringNonEmpty, $companyName: StringNonEmpty, $quantity: IntPositive, $costPerUnit: FloatNonNegative, $pricePerUnit: FloatNonNegative, $discount: Float0To1, $manufactureAt: DatePast, $expireAt: DateFuture) {
+    updateItem(organizationId: $organizationId, branchId: $branchId, id: $updateItemId, brandName: $brandName, companyName: $companyName, quantity: $quantity, costPerUnit: $costPerUnit, pricePerUnit: $pricePerUnit, discount: $discount, manufactureAt: $manufactureAt, expireAt: $expireAt)
   }
 `
 
@@ -124,6 +135,65 @@ function AddItem({ organizationId, branchId, refreshItems }: ItemsEditorProps) {
   )
 }
 
+function UpdateItem({ organizationId, branchId, refreshItems }: ItemsEditorProps) {
+  const formRef = useRef<HTMLFormElement>(null)
+  const [updateItem] = useMutation<UpdateItemMutation, UpdateItemMutationVariables>(UPDATE_ITEM)
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    event.stopPropagation()
+    if(!formRef.current) return
+    const form = new FormData(formRef.current)
+    const expireAt = form.get('expireAt')
+    const manufactureAt = form.get('manufactureAt')
+    const variables: UpdateItemMutationVariables = {
+      organizationId,
+      branchId,
+      updateItemId: form.get('updateItemId')?.toString() || '',
+      brandName: form.get('brandName')?.toString() || undefined,
+      companyName: form.get('companyName')?.toString() || undefined,
+      quantity: Number(form.get('quantity')?.toString()) || undefined,
+      costPerUnit: Number(form.get('costPerUnit')?.toString()) || undefined,
+      pricePerUnit: Number(form.get('pricePerUnit')?.toString()) || undefined,
+      discount: Number(form.get('discount')?.toString()) || undefined,
+      manufactureAt: manufactureAt !== null ? new Date(manufactureAt.toString()).getTime() : undefined,
+      expireAt: expireAt !== null ? new Date(expireAt.toString()).getTime() : undefined,
+    }
+    toast.promise<unknown, GraphQLError>(
+      queuePromise([
+        () => updateItem({ variables }),
+        () => refreshItems()
+      ]) ,
+      {
+        pending: 'Updating Item',
+        success: 'Item Updated',
+        error: {
+          render(error){
+            return error.data?.message
+          }
+        }
+      }
+    )
+  }
+
+  return (
+    <div className={styles.addItemEditor}>
+      <form onSubmit={handleSubmit} ref={formRef}>
+        <input required type='text' placeholder='Item ID*' name='updateItemId'/>
+        <input type='text' placeholder='Brand Name' name='brandName'/>
+        <input type='text' placeholder='Company Name' name='companyName'/>
+        <input type='number' step={1} min={1} placeholder='Quantity' name='quantity'/>
+        <input type='number' step={0.01} placeholder='Cost Per Unit' name='costPerUnit'/>
+        <input type='number' step={0.01} placeholder='Price Per Unit' name='pricePerUnit'/>
+        <input type='number' step={0.01} min={0} max={1} placeholder='Discount' name='discount'/>
+        <input type='date' placeholder='Manufacture At' name='manufactureAt' />
+        <input type='date' placeholder='Expire At' name='expireAt' />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
 function ItemsEditor(props: ItemsEditorProps) {
   
   const optionClassName = ({ isActive }: { isActive: boolean, isPending: boolean }) => `${styles.option} ${isActive && styles.active}`
@@ -132,13 +202,11 @@ function ItemsEditor(props: ItemsEditorProps) {
     <div className={styles.itemsEditorContainer}>
       <div className={styles.itemEditorOptions}>
         <NavLinkPersist className={optionClassName} to='item-add'>Add Item</NavLinkPersist>
-        <NavLinkPersist className={optionClassName} to='item-delete'>Delete Item</NavLinkPersist>
         <NavLinkPersist className={optionClassName} to='item-update'>Update Item</NavLinkPersist>
       </div>
       <Routes>
         <Route path='item-add' element={<AddItem {...props}/>}/>
-        <Route path='item-delete' element={<p>Delete Item!</p>}/>
-        <Route path='item-update' element={<p>Update Item!</p>}/>
+        <Route path='item-update' element={<UpdateItem {...props}/>}/>
       </Routes>
     </div>
   )
@@ -218,11 +286,11 @@ export function StockPage({ organizationId }: StockPageProps) {
                 <td>{item.id}</td>
                 <td>{item.brandName}</td>
                 <td>{item.quantity}</td>
-                <td>{item.costPerUnit}</td>
-                <td>{item.pricePerUnit}</td>
-                <td>{item.marginPerUnit}</td>
-                <td>{item.discount}</td>
-                <td>{item.profitLossPerUnit}</td>
+                <td>{currency.format(item.costPerUnit)}</td>
+                <td>{currency.format(item.pricePerUnit)}</td>
+                <td>{currency.format(item.marginPerUnit)}</td>
+                <td>{percent.format(item.discount)}</td>
+                <td>{currency.format(item.profitLossPerUnit)}</td>
               </tr>
             ))
           }
