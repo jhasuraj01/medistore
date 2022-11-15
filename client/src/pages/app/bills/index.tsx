e030edefc2835470870d14d4daea8b21937cdf75
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2022-11-15 20:49:42 +0000

    refactored

--- client/src/pages/app/bills/index.tsx
+++ client/src/pages/app/bills/index.tsx
@@ -37,6 +37,28 @@ function BranchesPageSubNav({organizationId}: {organizationId: string}) {
   })
 
   return (
+    <SubNav title='Branches' className={loading ? 'loading-top' : undefined}>
+      <SubNavSection>
+        {
+          data?.branches.map(branch => {
+            return (
+              <SubNavLink key={branch.id} to={branch.id}>{branch.name}</SubNavLink>
+            )
+          })
+        }
+      </SubNavSection>
+    </SubNav>
+  )
+}
+
+function BillsSubNav({organizationId}: {organizationId: string}) {
+  const { loading, data } = useQuery<GetBranchesQuery, GetBranchesQueryVariables>(GET_BRANCHES, {
+    variables: {
+      organizationId: organizationId
+    }
+  })
+
+  return (
     <SubNav title='Customer Bills' className={loading ? 'loading-top' : undefined}>
       <SubNavSection>
         {
@@ -81,7 +103,9 @@ export function BillsPage() {
     <Routes>
       <Route path='/' element={<AppSectionLayout subnav={<BranchesPageSubNav {...{organizationId}}/>}  />} >
         <Route index element={<BillsHomePage />} />
-        <Route path=':branchId/*' element={<BillPage />} />
+        <Route path=':branchId' element={<AppSectionLayout subnav={<BillsSubNav  {...{organizationId}}/>} />}>
+          <Route path=':billId' element={<BillPage />} />
+        </Route>
         <Route path='*' element={<NotFoundPage />} />
       </Route>
     </Routes>
